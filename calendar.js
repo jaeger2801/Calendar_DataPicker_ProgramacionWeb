f43f00a33094fb74cb4a517deb52ad18c33fb8b2
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-09-08 10:59:04 -0500

    Carga imagaenes

--- calendar.js
+++ calendar.js
@@ -6,7 +6,23 @@ const next_mth_element = document.querySelector('.date-picker .dates .month .nex
 const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
 const days_element = document.querySelector('.date-picker .dates .days');
 
+const images = [JanuaryImg, ]
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function preload() {
+	JanuaryImg = new loadImage('src/Jiraiya_img.png');
+	FebruaryImg = new loadImage('src/February_img.png');
+	MarchImg = new loadImage('src/March_img.png');
+	AprilImg = new loadImage('src/April_img.png');
+	MayImg = new loadImage('src/May_img.png');
+	JuneImg = new loadImage('src/June_img.png');
+	JulyImg = new loadImage('src/July_img.png');
+	AugustImg = new loadImage('src/August_img.png');
+	SeptemberImg = new loadImage('src/September_img.png');
+	OctoberImg = new loadImage('src/October_img.png');
+	NovemberImg = new loadImage('src/November_img.png');
+	DecemberImg = new loadImage('src/December_img.png');
+}
 
 let date = new Date();
 let day = date.getDate();
@@ -27,6 +43,7 @@ mth_element.textContent = months[month] + ' ' + year;
 
 selected_date_element.textContent = formatDate(date);
 selected_date_element.dataset.value = selectedDate;
+
 
 populateDates();
 
